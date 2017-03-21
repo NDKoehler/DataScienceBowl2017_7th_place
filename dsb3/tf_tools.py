463dd61033dc0e2021ca04ad41c86b9a8c9d70e5
falexwolf <alex.wolf@helmholtz-muenchen.de>
2017-03-21 18:59:43 +0100

    redirected all tensorflow stdout

--- dsb3/tf_tools.py
+++ dsb3/tf_tools.py
@@ -1,6 +1,7 @@
 import os, sys
 import logging
 import json
+from contextlib import contextmanager
 from . import pipeline as pipe
 
 def load_network(checkpoint_dir, image_shape=None, reuse=None):
@@ -26,19 +27,21 @@ def load_network(checkpoint_dir, image_shape=None, reuse=None):
     config['VARIABLES_TO_RESTORE'] = tf.contrib.slim.get_variables_to_restore()
     config['UPDATE_OPS_COLLECTION'] = tf.GraphKeys.UPDATE_OPS
     init_op = tf.global_variables_initializer()
-    sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=pipe.GPU_memory_fraction),
-                                            allow_soft_placement=True,
-                                            log_device_placement=config['allow_soft_placement']))
+    with redirected_stdout():
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=pipe.GPU_memory_fraction),
+                                                allow_soft_placement=True,
+                                                log_device_placement=config['allow_soft_placement']))
     sess.run(init_op)
     data = {'images': tf.placeholder(dtype=tf.float32, shape=[None] + image_shape, name='image_placeholder')}
-    with tf.device('/gpu:' + os.environ['CUDA_VISIBLE_DEVICES']):
-        config['dropout'] = 1.0
-        output, endpoints = model(data=data,
-                                  reuse=reuse,
-                                  restore_logits=False,
-                                  is_training=False,
-                                  scope=config['model_name'],
-                                  **config)
+    with redirected_stdout(pipe.log_tf):
+        with tf.device('/gpu:' + os.environ['CUDA_VISIBLE_DEVICES']):
+            config['dropout'] = 1.0
+            output, endpoints = model(data=data,
+                                      reuse=reuse,
+                                      restore_logits=False,
+                                      is_training=False,
+                                      scope=config['model_name'],
+                                      **config)
      # add all endpoints to predict
     config['endpoints'] = ['probs', 'logits']
     pred_ops = {}
@@ -56,6 +59,29 @@ def load_network(checkpoint_dir, image_shape=None, reuse=None):
         if var.name.split('/')[0] == config['model_name']:
             var_lst.append(var)
     saver = tf_saver.Saver(var_lst, write_version=tf.train.SaverDef.V2)
-    # TODO: This outputs so many variables... how can we suppress that?
-    saver.restore(sess, ckpt.model_checkpoint_path)
+    with redirected_stdout(pipe.log_tf):
+        # the following outputs all variables in the graph from _pywrap_tensorflow.so
+        # when calling _pywrap_tensorflow.TF_ExtendGraph
+        # this is why we use redirected_std_out() context manager
+        saver.restore(sess, ckpt.model_checkpoint_path)
     return sess, pred_ops, data
+
+@contextmanager
+def redirected_stdout(to=os.devnull):
+    """from http://stackoverflow.com/questions/5081657/how-do-i-prevent-a-c-shared-library-to-print-on-stdout-in-python
+    """
+    fd = sys.stdout.fileno()
+
+    def _redirect_stdout(to):
+        sys.stdout.close() # + implicit flush()
+        os.dup2(to.fileno(), fd) # fd writes to 'to' file
+        sys.stdout = os.fdopen(fd, 'w') # Python writes to fd
+
+    with os.fdopen(os.dup(fd), 'w') as old_stdout:
+        with open(to, 'w') as file:
+            _redirect_stdout(to=file)
+        try:
+            yield # allow code to be run with the redirected stdout
+        finally:
+            # restore stdout. buffering and flags such as CLOEXEC may be different
+            _redirect_stdout(to=old_stdout)

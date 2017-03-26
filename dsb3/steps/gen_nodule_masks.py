a8970f2ac4fcf06883834b9af0b596842750c0fd
falexwolf <alex.wolf@helmholtz-muenchen.de>
2017-03-26 14:10:49 +0200

    updated gen_nodule_masks

--- dsb3/steps/gen_nodule_masks.py
+++ dsb3/steps/gen_nodule_masks.py
@@ -96,18 +96,35 @@ def process_nodule_patient(patient, annotations, resample_lungs_json,
         patient_json['nodules'].append(nodule_json)
         # show the center of the annotation
         for crop in [False, True]:
-            if crop:
-                cropy = nodule_box_xyz_px[2], nodule_box_xyz_px[3] + 1 # convention is that bounding box includes the last point, but does not go beyond it
-                cropx = nodule_box_xyz_px[4], nodule_box_xyz_px[5] + 1
-            else:
-                cropy = [0, img_array_zyx.shape[1]]
-                cropx = [0, img_array_zyx.shape[2]]
-            plt.imshow(img_array_zyx[v_center_zyx_px[0], cropy[0]:cropy[1], cropx[0]:cropx[1]] + 0.25, cmap='Greys')
             color = 'r' if nodule_json['nodule_priority'] >= 3 else 'orange' if nodule_json['nodule_priority'] == 2 else 'green'
             level = 255 if nodule_json['nodule_priority'] >= 3 else 170 if nodule_json['nodule_priority'] == 2 else 85
-            plt.contour(mask_array_zyx[v_center_zyx_px[0], cropy[0]:cropy[1], cropx[0]:cropx[1], 0], colors=color, levels=[level-1], linestyles='dashed')
-            plt.contour(mask_array_zyx[v_center_zyx_px[0], cropy[0]:cropy[1], cropx[0]:cropx[1], 1], colors=color, levels=[level-1])
-            plt.savefig(pipe.get_step_dir() + 'figs/' + patient + '-nodule' + str(nodule_id) + '_crop' + str(int(crop)) + '.jpg')            
+            if crop:
+                cropz = max(0, nodule_box_xyz_px[0] - 20), min(nodule_box_xyz_px[1] + 20, img_array_zyx.shape[0]) # convention is that bounding box includes the last point, but does not go beyond it
+                cropy = max(0, nodule_box_xyz_px[2] - 20), min(nodule_box_xyz_px[3] + 20, img_array_zyx.shape[1]) # convention is that bounding box includes the last point, but does not go beyond it
+                cropx = max(0, nodule_box_xyz_px[4] - 20), min(nodule_box_xyz_px[5] + 20, img_array_zyx.shape[2])
+            else:
+                cropz = [0, img_array_zyx.shape[0]]
+                cropy = [0, img_array_zyx.shape[1]]
+                cropx = [0, img_array_zyx.shape[2]]
+            # view plane z
+            plt.imshow(img_array_zyx[v_center_zyx_px[0], cropy[0]:cropy[1], cropx[0]:cropx[1]] + 0.25, cmap='gray')
+            try:
+                plt.contour(mask_array_zyx[v_center_zyx_px[0], cropy[0]:cropy[1], cropx[0]:cropx[1], 0], colors=color, levels=[level-1], linestyles='dashed')
+                plt.contour(mask_array_zyx[v_center_zyx_px[0], cropy[0]:cropy[1], cropx[0]:cropx[1], 1], colors=color, levels=[level-1])
+            except ValueError:
+                pipe.log.warning(patient + ' raises ValueError in contour plot ' + str(nodule_id) + ' view plane z')
+                pipe.log.warning('    nodule_box_xyz_px' + str(nodule_box_xyz_px))
+                pipe.log.warning('    cropy ' + str(cropy) + ' cropx ' + str(cropx))
+            plt.savefig(pipe.get_step_dir() + 'figs/' + patient + '-nodule' + str(nodule_id) + '_zcrop' + str(int(crop)) + '.jpg')
+            plt.clf()
+            # view plane y
+            plt.imshow(img_array_zyx[cropz[0]:cropz[1], v_center_zyx_px[1], cropx[0]:cropx[1]] + 0.25, cmap='gray')
+            try:
+                plt.contour(mask_array_zyx[cropz[0]:cropz[1], v_center_zyx_px[1], cropx[0]:cropx[1], 0], colors=color, levels=[level-1], linestyles='dashed')
+                plt.contour(mask_array_zyx[cropz[0]:cropz[1], v_center_zyx_px[1], cropx[0]:cropx[1], 1], colors=color, levels=[level-1])
+            except ValueError:
+                pipe.log.warning(patient + ' raises ValueError in contour plot ' + str(nodule_id) + ' view plane y')
+            plt.savefig(pipe.get_step_dir() + 'figs/' + patient + '-nodule' + str(nodule_id) + '_ycrop' + str(int(crop)) + '.jpg')
             plt.clf()
     patient_json['basename'] = basename = patient + '_mask.npy'
     patient_json['mask_path'] = pipe.save_array(basename, mask_array_zyx)
@@ -165,7 +182,31 @@ def make_nodule(patient, nodule_annotations,
 
     # draw full thickness if thickness == -1, otherwise restrict to thickness 2
     small_enough = v_diam_zyx_px[0] * v_diam_zyx_px[1] * v_diam_zyx_px[2] <= 1000    
-    thickness = -1 if small_enough or ellipse_mode else 2
+    thickness = -1 if (small_enough or not ellipse_mode) else 2
+    draw_ellipses_in_layers(nodule_annotations, new_mask_array_zyx_shell, affected_layers, z_min_px, z_max_px, bound_box_offset_zyx_px, real_spacing_zyx, origin_zyx, thickness, nodule_priority_uint8, factor=1)
+    # draw reduced mask
+    if not ellipse_mode:
+        bbox_px_shell_zyx = get_bounding_box(new_mask_array_zyx_shell)
+        bbox_px_center_zyx = get_bounding_box(new_mask_array_zyx_shell) # If we do not fit ellipsoids, take the shell annotation also for the center.
+        # draw the nodule center by dividing radii by 2
+        draw_ellipses_in_layers(nodule_annotations, new_mask_array_zyx_center, affected_layers, z_min_px, z_max_px, bound_box_offset_zyx_px, real_spacing_zyx, origin_zyx, thickness, nodule_priority_uint8, factor=0.5)
+    # draw ellipsoid and get bounding boxes
+    else:
+        result = fit_ellipsoid(new_mask_array_zyx_shell, nodule_priority_uint8, v_center_zyx_px, v_diam_zyx_px)
+        new_mask_array_zyx_shell, bbox_px_shell_zyx, new_mask_array_zyx_center, bbox_px_center_zyx = result
+    # Ensure nodule priority in case of overlap - always take maximum
+    mask_array_zyx[:, :, :, 0] = np.maximum(new_mask_array_zyx_shell, mask_array_zyx[:, :, :, 0])
+    mask_array_zyx[:, :, :, 1] = np.maximum(new_mask_array_zyx_center, mask_array_zyx[:, :, :, 1])
+    return mask_array_zyx, v_center_zyx_px, center_anno_zyx_mm, v_diam_zyx_px, old_diameter_mm, bbox_px_shell_zyx, bbox_px_center_zyx
+
+def draw_ellipses_in_layers(nodule_annotations, new_mask_array_zyx, affected_layers, z_min_px, z_max_px, bound_box_offset_zyx_px, real_spacing_zyx, origin_zyx, thickness, nodule_priority_uint8, factor=1):
+    """Draw into new_mask_array_zyx."""
+    if factor < 1:
+        z_radius = factor * (z_max_px - z_min_px) / 2
+        z_mean = (z_max_px + z_min_px) / 2
+        z_radius = max(2, z_radius)
+        z_max_px = z_mean + z_radius
+        z_min_px = z_mean - z_radius
     for idx, v_layer in enumerate(affected_layers):
         # in z, y, x
         radius_decrease = 1 # decrease radii factor
@@ -176,25 +217,27 @@ def make_nodule(patient, nodule_annotations,
             # position of the v_layer in mm
             CoordZ_mm = (v_layer + bound_box_offset_zyx_px[0]) * real_spacing_zyx[0] + origin_zyx[0]
             # taking the annotation that is closest to the layer´s z-position
-            x_rad = nodule_annotations['diameter_x_px'].iloc[(nodule_annotations['coordZ'] - CoordZ_mm).abs().argsort().iloc[0]] // 2
-            y_rad = nodule_annotations['diameter_y_px'].iloc[(nodule_annotations['coordZ'] - CoordZ_mm).abs().argsort().iloc[0]] // 2
+            x_rad = factor * nodule_annotations['diameter_x_px'].iloc[(nodule_annotations['coordZ'] - CoordZ_mm).abs().argsort().iloc[0]] // 2
+            y_rad = factor * nodule_annotations['diameter_y_px'].iloc[(nodule_annotations['coordZ'] - CoordZ_mm).abs().argsort().iloc[0]] // 2
             x_center = nodule_annotations['coordX_px'].iloc[(nodule_annotations['coordZ'] - CoordZ_mm).abs().argsort().iloc[0]]
             y_center = nodule_annotations['coordY_px'].iloc[(nodule_annotations['coordZ'] - CoordZ_mm).abs().argsort().iloc[0]]
             radii_yx = (int(y_rad), int(x_rad)) # TODO: check
         # Case 2: z does not have its x_rad and y_rad, due to buffering
         else:
+            if factor < 1:
+                continue
             if v_layer < z_min_px:
                 # print('below')
-                x_rad = nodule_annotations['diameter_x_px'].ix[nodule_annotations['coordZ'].idxmin()] // 2
-                y_rad = nodule_annotations['diameter_y_px'].ix[nodule_annotations['coordZ'].idxmin()] // 2
+                x_rad = factor * nodule_annotations['diameter_x_px'].ix[nodule_annotations['coordZ'].idxmin()] // 2
+                y_rad =  factor * nodule_annotations['diameter_y_px'].ix[nodule_annotations['coordZ'].idxmin()] // 2
                 x_center = nodule_annotations['coordX_px'].ix[nodule_annotations['coordZ'].idxmin()]
                 y_center = nodule_annotations['coordY_px'].ix[nodule_annotations['coordZ'].idxmin()]
                 radii_yx = (int(round(np.sqrt(max(1, y_rad**2 - (radius_decrease*(z_min_px - v_layer))**2)))), # TODO: check!
                                    int(round(np.sqrt(max(1, x_rad**2 - (radius_decrease*(z_min_px - v_layer))**2)))))
             elif v_layer > z_max_px:
                 # print('above')
-                x_rad = nodule_annotations['diameter_x_px'].ix[nodule_annotations['coordZ'].idxmax()] // 2
-                y_rad = nodule_annotations['diameter_y_px'].ix[nodule_annotations['coordZ'].idxmax()] // 2
+                x_rad = factor * nodule_annotations['diameter_x_px'].ix[nodule_annotations['coordZ'].idxmax()] // 2
+                y_rad = factor * nodule_annotations['diameter_y_px'].ix[nodule_annotations['coordZ'].idxmax()] // 2
                 x_center = nodule_annotations['coordX_px'].ix[nodule_annotations['coordZ'].idxmax()]
                 y_center = nodule_annotations['coordY_px'].ix[nodule_annotations['coordZ'].idxmax()]
                 radii_yx = (int(round(np.sqrt(max(1, y_rad**2 - (radius_decrease * (z_max_px - v_layer))**2)))), # TODO: check
@@ -203,27 +246,15 @@ def make_nodule(patient, nodule_annotations,
                 log.pipe.warning('untreated layer in patient', patient)
         radii_yx = tuple([max(2, v) for v in radii_yx])
         # get an array for drawing with cv2
-        draw_array = new_mask_array_zyx_shell[v_layer, :, :].copy()
+        draw_array = new_mask_array_zyx[v_layer, :, :].copy()
         # for calling cv2.ellipse, need to change the convention to xy for center and axes but keep the array the same (i.e. yx)
         center_draw_xy = (x_center, y_center)
         radii_draw_xy = (int(radii_yx[1]), int(radii_yx[0]))
         cv2.ellipse(draw_array, center=center_draw_xy, axes=radii_draw_xy,
                     angle=0, startAngle=0, endAngle=360, color=(nodule_priority_uint8), thickness=thickness)
-        new_mask_array_zyx_shell[v_layer, :, :] = np.clip(draw_array + new_mask_array_zyx_shell[v_layer, :, :], 0, 255)
+        new_mask_array_zyx[v_layer, :, :] = np.clip(draw_array + new_mask_array_zyx[v_layer, :, :], 0, 255)
 
-    if ellipse_mode:
-        result = draw_ellipse(new_mask_array_zyx_shell, nodule_priority_uint8, v_center_zyx_px, v_diam_zyx_px)
-        new_mask_array_zyx_shell, bbox_px_shell_zyx, new_mask_array_zyx_center, bbox_px_center_zyx = result
-    else:
-        bbox_px_shell_zyx = get_bounding_box(new_mask_array_zyx_shell)
-        bbox_px_center_zyx = get_bounding_box(new_mask_array_zyx_shell) # If we do not fit ellipsoids, take the shell annotation also for the center.
-
-    # Ensure nodule priority in case of overlap - always take maximum
-    mask_array_zyx[:, :, :, 0] = np.maximum(new_mask_array_zyx_shell, mask_array_zyx[:, :, :, 0])
-    mask_array_zyx[:, :, :, 1] = np.maximum(new_mask_array_zyx_center, mask_array_zyx[:, :, :, 1])
-    return mask_array_zyx, v_center_zyx_px, center_anno_zyx_mm, v_diam_zyx_px, old_diameter_mm, bbox_px_shell_zyx, bbox_px_center_zyx
-
-def draw_ellipse(mask_array_zyx, color, v_center_px, v_diam_px):
+def fit_ellipsoid(mask_array_zyx, color, v_center_px, v_diam_px):
     v_diam_px = 2 * v_diam_px
     X = np.argwhere(mask_array_zyx > 1)
     center, radii, rotation = getMinVolEllipse(X, tolerance=0.01, v_center_px=v_center_px, v_diam_px=v_diam_px)
@@ -256,9 +287,9 @@ def draw_new_ellipsoid(new_mask_shape, center, radii, rotation, v_center_px, v_d
 def get_bounding_box(array):
     points = np.argwhere(array > 1)
     if len(points) > 0:        
-        bbox = [np.min(points[:, 0]), np.max(points[:, 0]),
-                np.min(points[:, 1]), np.max(points[:, 1]),
-                np.min(points[:, 2]), np.max(points[:, 2])]
+        bbox = [np.min(points[:, 0]), np.max(points[:, 0]) + 1,
+                np.min(points[:, 1]), np.max(points[:, 1]) + 1,
+                np.min(points[:, 2]), np.max(points[:, 2]) + 1]
     else:
         bbox = []
     return bbox
